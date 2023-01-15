8a60a0b73888e5aabadd64bd4fa718cb271662ec
unknown <{T3sultan}@users.noreply.github.com>
2023-01-14 20:39:47 -0800

    redux middleware

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "@redux-devtools/extension";
+import rootReducer from "./reducers/rootReducer";
+import logger from "redux-logger";
+import cartCounter from "./middleware/cartCounter";
+import thunk from "redux-thunk";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(cartCounter, thunk,logger))
+);
+
+export default store;
